21c964d96451ff5d48e2d8406bfbe4ded0263a85
marcinzygan <soul@o2.pl>
2022-05-10 19:11:29 +0100

    added shapes in work section

--- src/Components/WorkSection.js
+++ src/Components/WorkSection.js
@@ -1,13 +1,15 @@
-import Heading from "./Heading"
+import React from "react"
 import { ProjectsData } from "./ProjectsData"
+import Heading from "./Heading"
 import Project from "./Project"
 import Modal from "./Modal"
-import React from "react"
+import Shapes from "../Images/about__shapes.svg"
 
 
 
 
 export default function WorkSection(){
+
 
 // Set the state for the dynamic change of the content inside modal 
 const [modalContent , setModalContent] = React.useState([])
@@ -24,8 +26,10 @@ const modalContentChange = (data) => {
     setIsModalOpen(prevState => !prevState)
     }
    
-    //Map over the data array any create <Project> component for each item . 
-const allProjects = ProjectsData.map(data => 
+
+//Map over the data array any create <Project> component for each item . 
+
+    const allProjects = ProjectsData.map(data => 
     <Project
         key={data.id}
         projectImg={data.mainImage}
@@ -33,39 +37,43 @@ const allProjects = ProjectsData.map(data =>
         alt={data.name}
         projectName={data.name}
         projectContent={data.content}
-        projectContent2={data.content2}
-        
+        projectContent2={data.content2} 
         onClick={()=>modalContentChange(data)}  
     />
     )
 
 
-    return(
-    <section id="work" className="work__section" >
-
+return(
+<section id="work" className="work__section" >
+    
         <Heading
         sectionHeading={"My Work"}
         sectionSubHeading={"Projects that I am proud of ."}
         />
+    <div className="work__container">
 
-    <div className="project__background">
-        {/* Displays all data mapped as a project component */}
-        {allProjects}
+        <div className="project__background">
+            {/* Displays all data mapped as a project component */}
+            {allProjects}
 
-        {/* Conditionaly renders modal if its true (open) */}
-        {/* {isModalOpen && */}
-        <Modal
-        modalContent={modalContent}
-        onClick={modalToggle}
-        isModalOpen={isModalOpen}
-        />
-        
-        
+            {/* Conditionaly renders modal if its true (open) */}
+            {/* {isModalOpen && */}
+            <Modal
+            modalContent={modalContent}
+            onClick={modalToggle}
+            isModalOpen={isModalOpen}
+            />
+        </div>
+
+        <div className="about__shapes-container">
+            <img src={Shapes} alt="geometric shapes" className="work__shapes-img"></img>
+        </div>
+    </div>
+
+    <div className="section__number-container">
+        <p className="section__number">01</p>
     </div>
     
-    <div className="section__number-container">
-    <p className="section__number">01</p>
-        </div>
-    </section>
+</section>
     )
 }
