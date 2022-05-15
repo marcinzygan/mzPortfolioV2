83f35d8cd5d1765590ff504026e4148f41954bb4
marcinzygan <soul@o2.pl>
2022-05-15 19:06:48 +0100

    added images and data for work section

--- src/Components/ProjectsData.js
+++ src/Components/ProjectsData.js
@@ -2,10 +2,13 @@ export const ProjectsData = [
   {
     id: 1,
     name: "Shipmodels",
-    mainImage: require("../Images/hero1.jpg"),
+    mainImage: require("../Images/Projects/Shipmodels/shipintro.jpg"),
     images: [
-      "https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-      "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      require("../Images/Projects/Shipmodels/ship1.jpg"),
+      require("../Images/Projects/Shipmodels/ship2.jpg"),
+      require("../Images/Projects/Shipmodels/ship3.jpg"),
+      require("../Images/Projects/Shipmodels/ship4.jpg"),
+      require("../Images/Projects/Shipmodels/ship5.jpg"),
     ],
     contentTitle:
       "Shipmodels is a company that specializes in building quality and detailed models of ships .",
@@ -37,15 +40,36 @@ export const ProjectsData = [
 
   {
     id: 2,
-    name: "Project 2",
-    mainImage: require("../Images/hero2.jpg"),
+    name: "Rebelinska Art",
+    mainImage: require("../Images/Projects/AR art/arintro.jpg"),
     images: [
-      "https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-      "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      "https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/b39ca4132934855.61b29bceee05c.jpg",
+      require("../Images/Projects/AR art/ar1.jpg"),
+      require("../Images/Projects/AR art/ar2.jpg"),
+      require("../Images/Projects/AR art/ar3.jpg"),
+      require("../Images/Projects/AR art/ar4.jpg"),
     ],
-    contentTitle: "Hello World",
-    content:[ "lorem kukududud",],
+    contentTitle: "Rebelinska Art is a portfolio website designed to showcase the client recent work .",
+    content:[ "I wanted to keep a simple and elegant look for the website to fit the art theme of this project.",
+              "The website is built in responsive technology.",
+              "First I've designed the website in Figma , and later on the design was approved by client.",
+              "Website was build in HTML , CSS and JavaScript.",
+  ],
+  listTitle: "Features implemented :",
+  list: {
+    item1: "Figma design for desktop and mobile screens",
+    item2: "Website built in Html",
+    item3: "CSS styling",
+    item4: "Mobile and desktop responsive via CSS Media queries",
+    item5: "Mobile navbar (hamburger menu) JS",
+    item6: "Back to home button JS",
+    item7: "Dynamic date set in footer JS",
+  },
+    links: [
+      {
+        name: "View live Demo",
+        link: "https://portfoliomzdesign.netlify.app",
+      },
+    ],
   },
   {
     id: 3,
@@ -70,18 +94,26 @@ export const ProjectsData = [
   {
     id: 4,
     name: "Github Projects",
-    mainImage: require("../Images/project1.jpg"),
+    mainImage: require("../Images/Projects/Github/githubintro.jpg"),
     images: [
       "https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
       "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     ],
+    contentTitle:  "Some of my projects that I've done while learning.",
     content: [
-      "Some of my projects that I've done while learning.",
       "Javascript & React projects :",
     ],
     links: [
       { name: "Bankist App", link: "https://github.com/marcinzygan/BankApp" },
-      { name: "Github", link: "https://github.com/marcinzygan" },
+      { name: "Meme Generator", link: "https://github.com/marcinzygan/React-meme-generator" },
+      { name: "Tenzies Game ", link: "https://github.com/marcinzygan/React-Tenzies" },
+      { name: "Travel Journal", link: "https://github.com/marcinzygan/React-travel-Journal" },
+      { name: "Buisness Card", link: "https://github.com/marcinzygan/React-BusinessCard" },
+      { name: "Task Organizer", link: "https://github.com/marcinzygan/JavaScript-Task-Organizer" },
+      { name: "Color Flipper", link: "https://github.com/marcinzygan/JavaScript-Color-Flipper" },
+      { name: "Countdown Timer", link: "https://github.com/marcinzygan/JavaScript-Cuntdown-Timer" },
+      { name: "Guess Number Game", link: "https://github.com/marcinzygan/JavaScript-Guess-My-Number" },
+      { name: "Bankist Website", link: "https://github.com/marcinzygan/JavaScript-Bankist-Website" },
     ],
 
 
